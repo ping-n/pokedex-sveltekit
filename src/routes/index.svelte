1db87da9cef3c0b4d1aa29b4f6c24ce0b163671b
PING NGE <ping.nge@hotmail.com>
2021-06-05 21:38:02 +1000

    Added pokeMart store to fetch pokemon data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,13 @@
+<script>
+  import {pokemon} from "../stores/pokeMart"
+</script>
+
 <svelte:head>
 	<title>Svelte Kit Pokemon App</title>
 </svelte:head>
 
 <h1>Sevlte kit Pokedex</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each $pokemon as poke}
+  <p>{poke.name}</p>
+{/each}
---
+++ src/stores/pokeMart.js
@@ -0,0 +1,22 @@
+import { writable } from 'svelte/store';
+
+export const pokemon = writable([]);
+let loaded = false;
+
+const fetchPokemon = async () => {
+	if (loaded) return;
+	const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+	const res = await fetch(url);
+	const data = await res.json();
+	const loadedPokemon = data.results.map((data, index) => ({
+		name: data.name,
+		id: index + 1,
+		image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+			index + 1
+		}.png`
+	}));
+	pokemon.set(loadedPokemon);
+	loaded = true;
+};
+
+fetchPokemon();
